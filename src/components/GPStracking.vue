489f6fa5b4fb96077bc4e4db66240504cffd8978
Worawat Pha <worawat.pha@gmail.com>
2019-12-12 18:16:31 +0700

    .

---
+++ src/components/GPStracking.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <iframe id="iframe1" src="http://www.igpstracking.net/" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component export default class GPSTracking extends Vue {@Prop() private gpstrackingweb!: string;}
+</script>
---
+++ src/views/GPStracking.vue
@@ -0,0 +1,15 @@
+<template>
+    <GPSTracking gpstrackingweb="http://www.igpstracking.net/"/>
+</template>
+
+<script>
+// @ is an alias to /src
+import GPSTracking from '@/components/GPStracking.vue'
+
+export default {
+  name: 'gpstrackingweb',
+  components: {
+    GPSTracking
+  }
+}
+</script>
