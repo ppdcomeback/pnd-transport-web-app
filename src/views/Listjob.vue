a2c7c06fcf7b5a0e8cb96bc200ff985164047520
Worawat Pha <worawat.pha@gmail.com>
2020-01-17 13:15:15 +0700

    add print button (../compontnt/printPDFbuttom.vue)

--- src/views/Listjob.vue
+++ src/views/Listjob.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is a listjob page</h1>
-    <table class="table table-hover">
+  <div class="listjob">
+    <div class="row">
+      <div class="col">
+        <h1>This is a listjob page</h1>
+      </div>
+      <div class="col-2">
+        <printPDFbutton/>
+      </div>
+    </div>
+    <div>
+      <table class="table table-hover">
         <thListJob/>
         <trListJob/>
     </table>
+    </div>
   </div>
 </template>
 
 <script>
 import thListJob from '@/components/ListJob-table/tableListJob.vue'
 import trListJob from '@/components/ListJob-table/trListJob.vue'
+import printPDFbutton from '@/components/printPDFbutton.vue'
 export default {
   name: 'listjob',
   components: {
     thListJob,
-    trListJob
+    trListJob,
+    printPDFbutton
   }
 }
 </script>
