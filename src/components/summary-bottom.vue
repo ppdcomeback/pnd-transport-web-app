e6189572ae5edd9ad808ac593e3eb4132e67bb1a
Worawat Pha <worawat.pha@gmail.com>
2020-01-20 02:21:31 +0700

    summary bottom fix

--- src/components/summary-bottom.vue
+++ src/components/summary-bottom.vue
@@ -10,7 +10,7 @@
             <th class="text-right">หักภาษี ณ ที่จ่าย</th>
             <td>[sql]จำนวนเงิน</td>
         </tr>
-        <tr>
+        <tr class="table-active">
             <td colspan="7" class="text-center">[sql]"สรุปยอดเป็นตัวหนังสือภาษาไทย"</td>
             <td>[sql]ยอดชำระ</td>
         </tr>
